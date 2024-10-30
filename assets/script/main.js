16f25b54952e0e9cff00f0cb2646704b2637e918
Misato-0112 <misato@m0112.mygbiz.com>
2024-10-29 21:57:36 -0500

    10.30最終

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,23 +5,40 @@ $(document).ready(function () {
     });
 
     $(window).on("resize", function () {
-        // console.log('resize');
         hamburger.removeClass("hamburger-nav--active");
     });
+
+    if ($(".accordion").length > 0) {
+        $(".accordion__item").click(function () {
+            var parentItem = $(this);
+            var detail = parentItem.find(".accordion__detail");
+            var button = parentItem.find(".accordion-summary__button");
+
+            if (detail.hasClass("accordion__detail--active")) {
+                detail.removeClass("accordion__detail--active");
+                button.removeClass("accordion-summary__button--active");
+                parentItem.removeClass("accordion__item--active");
+            } else {
+                detail.addClass("accordion__detail--active");
+                button.addClass("accordion-summary__button--active");
+                parentItem.addClass("accordion__item--active");
+            }
+        });
+    }
+
+    if ($(".swiper").length > 0) {
+        const mySwiper = new Swiper(".swiper", {
+            slidesPerView: 1,
+            spaceBetween: 0,
+
+            pagination: {
+                el: ".swiper-pagination",
+            },
+
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    }
 });
-
-if (document.querySelector(".swiper")) {
-    const mySwiper = new Swiper(".swiper", {
-        slidesPerView: 1,
-        spaceBetween: 0,
-
-        pagination: {
-            el: ".swiper-pagination",
-        },
-
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-}
